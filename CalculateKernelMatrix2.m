76b1f7e2aa47ef04aae2c28fd96957143e1e3cbe
rakesh1000 <31788388+rakesh1000@users.noreply.github.com>
2018-04-07 19:10:20 +0530

    Delete CalculateKernelMatrix2.m

--- CalculateKernelMatrix2.m
+++
@@ -1,3 +0,0 @@
-function result=CalculateKernelMatrix2(data1,data2,theta,ker_vec1,ker_vec2,CoeffMatrix)
-    result=CalculateKernelMatrix1(data1,data2,theta)-ker_vec1'*CoeffMatrix*ker_vec2;
-    clear data1 data2 theta ker_vec1 ker_vec2 CoeffMatrix;
